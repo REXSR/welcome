679e9f17cb18bd292681bc40ec0dba952d552015
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-18 10:04:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,7 +137,7 @@ channel.guild.owner.send(`<@!${channelremover.id}>
   },Otime)
   });
 
-let log = JSON.parse(fs.readFileSync("./logs.json", "utf8");
+let log = JSONparse(fs.readFileSync("./logs.json", "utf8");
 var defaultlogchannel = 'logs';
 var logs;
 if(log[message.guild.id]){
