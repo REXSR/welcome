bc09c2e26c26c382fcdf509bb046b168e461f63b
REXSR <41483292+REXSR@users.noreply.github.com>
2018-08-18 10:14:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,23 +137,68 @@ channel.guild.owner.send(`<@!${channelremover.id}>
   },Otime)
   });
 
-var defaultlogchannel = 'logs';
-var logs;
-if(log[message.guild.id]){
-var logs = log[message.guild.id].logs;
-}else{
-var logs = 'logs';
-}
-if(message.content.startsWith(prefix + "setlog")){
-if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")){return message.reply("x Vous n'avez pas les permissions dans ce serveur").catch(console.error);
-}else{
-let args = message.content.split(' ').slice(1);
-if(!args) return message.channel.send('x Merci de specifier un channel')
-log[message.guild.id] = {"logs": args.join(" ")};
-message.channel.send("Mon channel des logs d'annonce est "+ args.join(" ") + "");
-fs.writeFile("./logs.json", JSON.stringify(log), (err) => {if (err) console.error(err);});
-}
-}
+
+
+
+client.on('message', message => {
+
+if (message.content.startsWith('inv-info')) {
+
+let oi = message.mentions.users.first() ? message.mentions.users.first().id : message.author.id ; 
+
+  let img = message.mentions.users.first() ? message.mentions.users.first().username : message.author.username;
+
+  let imagemm = message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL
+
+  message.guild.fetchInvites().then(invs => {
+
+    let member = client.guilds.get(message.guild.id).members.get(oi);
+
+    let personalInvites = invs.filter(i => i.inviter.id === oi);
+
+    let urll = invs.filter(i => i.inviter.id === oi);
+
+    let link = urll.reduce((p , v) => v.url +` , Total de membros recrutados no convite: ${v.uses}.\n`+ p, `\nServidor: ${message.guild.name} \n `);
+
+    let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+
+   let exec = personalInvites.reduce((p, v) => v.inviter);
+
+ let possibleInvites = [['Total de membros recrutados:']];
+
+possibleInvites.push([inviteCount, exec]);
+
+        let user = message.mentions.users.first() || message.author;
+
+        let mem = message.guild.member(user);
+
+        let millisJoined = new Date().getTime() - mem.joinedAt.getTime();
+
+        let daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+
+const alpha = new Discord.RichEmbed()
+
+.setAuthor(img)
+
+.addField('🏆 Invite Infos',  `\n\n► لقد قمت بدعوة \`\`${Number(inviteCount)}\`\` عضو.\n\n► لقد انضممت لسرفر مند\`${daysJoined.toFixed(0)}\`يوم .\n\n► لقد انضممت بهذه الدعوة\`${exec}\``,true)
+
+.setThumbnail(imagemm)
+
+.setColor(0x4959e9);
+
+message.channel.send(alpha);
+
+});
+
+};
+
+  });
+
+
+
+
+
+
 
 
 
