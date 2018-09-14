2ba05b6c1c7b4fa04622be84d6121fbc20bc3049
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-15 00:13:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,33 +353,7 @@ client.on('voiceStateUpdate', (oldM, newM) => {
 
 
   
-client.on('message', message => {
-  if (message.author.kick) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-  if (command == "kick") {
-               if(!message.channel.guild) return;
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  if (message.mentions.users.size < 1) return message.reply("منشن شخص");
-  if(!reason) return message.reply ("اكتب سبب الطرد");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
-  message.guild.member(user).kick(7, user);
-  const banembed = new Discord.RichEmbed()
-  .setAuthor('Kicked !', user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("User:",  `[ + ${user.tag} + ]`)
-  .addField("By:", `[  + ${message.author.tag} +  ]`)
-  .addField("Reason:", `[ + ${reason} +  ]`)
-  client.channels.get("486877835839930368").send({embed : banembed})
-}
-});
+
 
 
 
@@ -1065,7 +1039,7 @@ client.on('message', message => {
   
   if (message.mentions.users.size < 1) return message.reply(":information_source:  #kick @NAWAF يجب تحديد شخص ");
   if (!message.guild.member(user)
-  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
+  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد طرده**");
 
 
   message.guild.member(user).kick(7, user);
