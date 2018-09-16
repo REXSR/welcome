576322fc769dd826abe152957dc18f3617f1ea86
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-17 00:06:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1068,17 +1068,60 @@ message.channel.send(`**:white_check_mark: ${user.tag} kicked from the server ! 
 
  
          
-
+let points = JSON.parse(fs.readFileSync('./points.json', 'utf8')); // يقوم بقراءه ملف النقاط , والمسار حق النقاطس العام لجميع الأوامر
 client.on('message', message => {
-  if (message.content.startsWith("replay")) { 
-    if (!serverQueue) return message.channel.send('لا يوجد شيء حالي ف العمل');
-    const embedNP = new Discord.RichEmbed()
-.setDescription(`سيتم اعاده تشغيل الفديو :**${serverQueue.songs[0].title}**`)
-message.channel.send({embed: embedNP})
-return handleVideo(video, message, message.member.voiceChannel);
+if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+  };
+if (message.content.startsWith(prefix + 'فكك')) {
+    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+
+const type = require('./fkk.json');
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 15 ثانيه لتفكيك الكلمه**').then(msg => {
+let embed = new Discord.RichEmbed()
+.setColor("04791c")
+ .setImage(`${item.type}`)
+msg.channel.send(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+         const sh = new Discord.RichEmbed()
+.setColor("04791c")
+.setDescription('**✅ |Good Job +1P**')
+.addField('Type G.mypoints', 'To Show ur Points' , true)
+.setFooter(message.author.username, message.author.avatarURL)
+message.channel.sendEmbed(sh);
+        let won = collected.first().author;
+                points[won.id].points++;
+        })
+           .catch(collected => { // في حال لم يقم أحد بالإجابة
+            message.channel.send(`🔚 |**Time Is End**`);
+           })
+          fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+          })
+        })
+    })
 }
+})
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'نقاطي')) {
+	if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+	let userData = points[message.author.id];
+	let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+	.setColor('#000000')
+	.setDescription(`نقاطك: \`${userData.points}\``)
+	message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
 });
- 
+
 
                               
       
